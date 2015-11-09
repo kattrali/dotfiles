336f87d3843c9cd8ea589360f626e67e8664a9c9
Delisa Mason <iskanamagus@gmail.com>
2015-11-09 13:49:56 -0800

    [init] Fix buffers script install directory

--- init/config/weechat.sh
+++ init/config/weechat.sh
@@ -5,4 +5,6 @@ cd ~/.weechat/python/autoload
 
 wget https://raw.githubusercontent.com/rawdigits/wee-slack/master/wee_slack.py
 wget https://raw.githubusercontent.com/kattrali/weemoji/master/weemoji.py
+
+cd ~/.weechat/perl/autoload
 wget https://weechat.org/files/scripts/buffers.pl
