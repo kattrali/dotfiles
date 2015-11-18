1be57141e2593e94b297f5d406130c9b4403a423
Delisa Mason <iskanamagus@gmail.com>
2015-11-18 13:27:02 -0800

    [init] Add quicklook previews for markdown, disable stats

--- fish/config.fish
+++ fish/config.fish
@@ -7,10 +7,10 @@ set -x EDITOR vim
 set -x GEM_HOME $HOME/.local/gems/(ruby -v | awk '{print $2}')
 set -x GEM_PATH $GEM_HOME
 set -x HOMEBREW_BUILD_FROM_SOURCE 1
+set -x COCOAPODS_DISABLE_STATS 1
 set -x PATH $GEM_HOME/bin $HOME/bin $GOPATH/bin \
             /usr/local/bin /usr/local/sbin /usr/bin \
             /bin /usr/sbin /sbin /usr/local/go/bin
 
 source /usr/local/share/chruby/chruby.fish
 source $HOME/.config/fish/functions/aliases.fish
-
