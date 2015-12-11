87a0b34d25217060e8a5b928e5f8faf79e6fe02b
Delisa Mason <iskanamagus@gmail.com>
2015-12-10 16:29:01 -0800

    [fish] Set /usr/bin first in PATH
    - Fix intermittent clang issues with brewed llvm
      “stdarg.h not found”

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -8,8 +8,8 @@ set -x GEM_HOME $HOME/.local/gems/(ruby -v | awk '{print $2}')
 set -x GEM_PATH $GEM_HOME
 set -x HOMEBREW_BUILD_FROM_SOURCE 1
 set -x COCOAPODS_DISABLE_STATS 1
-set -x PATH $GEM_HOME/bin $HOME/bin $GOPATH/bin \
-            /usr/local/bin /usr/local/sbin /usr/bin \
+set -x PATH /usr/bin $GEM_HOME/bin $HOME/bin \
+            /usr/local/bin /usr/local/sbin $GOPATH/bin \
             /bin /usr/sbin /sbin /usr/local/go/bin
 
 source /usr/local/share/chruby/chruby.fish
