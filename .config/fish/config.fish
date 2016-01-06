6e9d89f60130a0385268246f18350f1a1bc535a3
Delisa Mason <iskanamagus@gmail.com>
2016-01-06 16:05:12 +0000

    [fish] Save the environment

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -6,7 +6,6 @@ set -x MAILDIR $HOME/mail
 set -x EDITOR vim
 set -x GEM_HOME $HOME/.local/gems/(ruby -v | awk '{print $2}')
 set -x GEM_PATH $GEM_HOME
-set -x HOMEBREW_BUILD_FROM_SOURCE 1
 set -x COCOAPODS_DISABLE_STATS 1
 set -x PATH /usr/bin $GEM_HOME/bin $HOME/bin \
             /usr/local/bin /usr/local/sbin $GOPATH/bin \
