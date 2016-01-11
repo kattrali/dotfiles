7b5487c5eac74d6a9d0d9d71f13e8d110bcd7884
Delisa Mason <iskanamagus@gmail.com>
2016-01-11 16:41:12 +0000

    [fish] Print task info with tomato

--- .config/fish/functions/tomato.fish
+++ .config/fish/functions/tomato.fish
@@ -1,9 +1,26 @@
 #!/usr/bin/env fish
+# A timer for pomodoro-alike working schedules
+# Usage:
+#
+#   tomato [task] # Starts a timer for $TOMATO_WORK_LEN
+#   tomato break  # Starts a time for $TOMATO_BREAK_LEN
 
 function tomato
-   if echo $argv | grep 'break' 1>/dev/null
-     utimer -t 5m; and say 'back to work!'
-   else
-     utimer -t 20m; and say 'done'
-   end
+  set TOMATO_BREAK_LEN 5m
+  if test (count $argv) -eq 0
+    _tomato_work
+  else if test (echo $argv[1]) = 'break'
+    echo "Take a break :)"
+    utimer -t $TOMATO_BREAK_LEN; and say 'back to work!'
+  else
+    echo "Next task: $argv[1..-1]"
+    _tomato_work
+  end
 end
+
+function _tomato_work
+  set TOMATO_WORK_LEN 20m
+  echo "Time for work!"
+  utimer -t $TOMATO_WORK_LEN; and say 'done'
+end
+
