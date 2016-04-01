c70ce498c3f3b3d3fccba9c5b486d7c3b7b836ca
Delisa Mason <iskanamagus@gmail.com>
2016-03-31 18:32:20 -0700

    [fish] Always use pip with virtualenv

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -7,6 +7,7 @@ set -x EDITOR vim
 set -x GEM_HOME $HOME/.local/gems/(ruby -v | awk '{print $2}')
 set -x GEM_PATH $GEM_HOME
 set -x COCOAPODS_DISABLE_STATS 1
+set -x PIP_REQUIRE_VIRTUALENV 1
 set -x PKG_CONFIG_PATH /usr/local/lib/pkgconfig /usr/local/lib
 set -x PATH $GEM_HOME/bin $HOME/bin /usr/local/bin /usr/local/sbin \
             /bin /usr/bin /usr/sbin /sbin $GOPATH/bin /usr/local/go/bin \
