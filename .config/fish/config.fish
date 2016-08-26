f6ab3b242d50b9a0510ea1aa4190b59644936208
Delisa Mason <iskanamagus@gmail.com>
2016-08-26 22:41:13 +0100

    [pager] Improve readability

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -4,6 +4,7 @@ set fish_greeting
 set -x GOPATH $HOME/Code/gopkg
 set -x MAILDIR $HOME/mail
 set -x EDITOR vim
+set -x MANPAGER vimpager
 set -x GEM_HOME $HOME/.local/gems/(ruby -v | awk '{print $2}')
 set -x GEM_PATH $GEM_HOME
 set -x COCOAPODS_DISABLE_STATS 1
