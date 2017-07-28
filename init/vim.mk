60d1a42e722588fda3e4d24883974091a0bde31c
Delisa Mason <iskanamagus@gmail.com>
2017-07-28 12:07:47 -0700

    [init] Exit vim after installing plugins

--- init/vim.mk
+++ init/vim.mk
@@ -5,4 +5,4 @@ all: $(PLUG)
 $(PLUG):
 	@install -d $(HOME)/.vim/autoload
 	@curl -fLo $(HOME)/.vim/autoload/plug.vim  https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-	@vim -c PlugInstall
+	@vim -c PlugInstall -c qall
